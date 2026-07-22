6377e75ee20e61e466720b880172c2e949c37f74
manuelfrechet <manuel.frechet@gmail.com>
2026-07-22 16:45:43 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -89,8 +89,8 @@ def parse_fit_to_tables(fit_source: Any) -> Dict[str, pd.DataFrame]:
         if not df.empty and "timestamp" in df.columns:
             df["timestamp"] = pd.to_datetime(df["timestamp"], errors="coerce")
 
-        if not df.empty:
-            df = df[_order_columns(list(df.columns))]
+        #if not df.empty:
+        #    df = df[_order_columns(list(df.columns))]
             
         dfs[message_type] = df
 
